ee48a3bc2923b3b3262e9d4349958674354ae26a
Gourav Roy <gouravroy7209@gmail.com>
2020-11-22 12:44:14 +0530

    feat:deleteApiAddedd

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.cruddemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -66,11 +67,11 @@ public class EmployeeRestController {
 	}
 	
 	
-//	@DeleteMapping("/deleteemployee/{id}")
-//	public String deleteEmployee(@PathVariable("id") int theId)
-//	{
-//		
-//		return employeeService.deleteEmployee(theId);
-//	}
+	@DeleteMapping("/deleteemployee/{id}")
+	public String deleteEmployee(@PathVariable("id") int theId)
+	{
+		
+		return employeeService.deleteEmployee(theId);
+	}
 
 }
